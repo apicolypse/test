6edc707d91e49e4c63cef6047b22807812e4b1d9
apicolypse <34241656+apicolypse@users.noreply.github.com>
2023-05-28 18:47:42 +0500

    connection.php

--- connection.php
+++ connection.php
@@ -1,10 +1,3 @@
 <?php
-
-$host="localhost";
-$db_user="hamza";
-$db_pass="root123";
-$db_name="businesshub";
-
-$conn = mysqli_connect($host , $db_user , $db_pass , $db_name );
-
+ $response='OK';
 ?>
